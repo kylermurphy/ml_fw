54ac03760ee1f82802857f062f80cd5d0d335fea
kylermurphy <34041071+kylermurphy@users.noreply.github.com>
2024-06-24 15:52:05 -0400

    Update ml_mod.py
    Added functionality for finding best model based on attribute or by combining the scorers past to the grid search

--- ml_fw/ml_mod.py
+++ ml_fw/ml_mod.py
@@ -5,7 +5,10 @@ Created on Thu Jun 13 14:54:54 2024
 @author: krmurph1
 """
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
 
 
 def train(f_dat: pd.DataFrame,
@@ -14,14 +17,15 @@ def train(f_dat: pd.DataFrame,
           est_params: dict = None,
           est_kwargs: dict = None,
           grid_params: dict = None,
-          grid_kwargs: dict = {}):
+          grid_kwargs: dict = {}, 
+          grid_ratio: float = 0.3):
 
     # if a set of grid parameters
     # has been sent then do a grid 
     # search to fine the best set of parameters
     
-### FIXME: need to add a size parameter/partition here
-
+    # identify the best estimator or the nominal set of fit 
+    # parameters to set the best estimator
     if grid_params:
         print('''
               Performing grid search using grid_params and using
@@ -30,26 +34,55 @@ def train(f_dat: pd.DataFrame,
               grid_kwargs variable. 
               
               Important Note: this can take a substantial amount of
-              time depending on the passed estimator              
+              time depending on the passed estimator.
+
         ''')
-        print('Performing Grid Search:')
         
-        est_tune = tune(estimator, grid_params,f_dat,y_dat, **grid_kwargs)
-        # check if the best estimator is present
-        if 'best_estimator_' in est_tune.__dict__:
-            best_est = est_tune.best_estimator_
-        else:
-            # FIXME: Need to find the best estimator.
-            best_est = None
+        if grid_ratio and grid_ratio < 1:
+            print(
+                f'Performing Grid Search using {grid_ratio*100:.2f}% of data')
+            x_grid, _, y_grid, _ = train_test_split(f_dat,y_dat, 
+                                                    train_size=grid_ratio)
+            est_tune = tune(estimator, grid_params,x_grid,y_grid, 
+                            **grid_kwargs)
+        else: 
+            print('Performing Grid Search using all data')
+            est_tune = tune(estimator, grid_params,f_dat,y_dat.values.ravel(), 
+                            **grid_kwargs)
             
-    
-    # decide if we're tuning the model
+        # check if the best estimator is present
+        # if it is use that estimator to fit the model
+        # if it isn't this is likely because multiple scores
+        # have been passed.
+        # if this is the case normalize the train and test 
+        # scores for each metric
+        # combine them using root mean square 
+        # sqrt(average(sum(score**2)))
+        if 'best_estimator_' in est_tune.__dict__:
+            est_fit = est_tune.best_estimator_
+        else:
+            dist = list()
+            scorers = list(est_tune.scorer_.keys())
+            average = ['mean_train_','mean_test_']
+            for sc in scorers:
+                for av in average:
+                    dist.append(est_tune.cv_results_[av+sc]) 
+            
+            scaler = MinMaxScaler()
+            scaler.fit(np.array(dist).transpose())
+            dist2 = scaler.transform(np.array(dist).transpose())**2
+            best_pos = np.sqrt(dist2.sum(axis=1)/dist2.shape[1]).argmax()
+            
+            # get the best parameters
+            # and set the final estimator for fitting
+            est_fit = estimator.set_params(
+                **est_tune.cv_results_['params'][best_pos])
     
     # fit the model
-    
+    est_fit.fit(f_dat, y_dat.values.ravel())
     # return the model
     
-    return est_tune
+    return est_fit
 
 def tune(estimator, 
          grid_param: dict,

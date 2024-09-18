ca0069b4ae98101cb39fda7bdeb5ee772cc25fd6
kylermurphy <34041071+kylermurphy@users.noreply.github.com>
2024-09-18 14:28:31 -0400

    Create resid.py
    New module for analysing residuals

---
+++ ml_fw/resid.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 18 11:28:59 2024
+
+@author: krmurph1
+
+
+ivestigate residuals of a ml model
+
+"""
+
+import pandas as pd
+
+
+def boxplot_vx(x_dat: pd.DataFrame | list, 
+               y_dat: pd.DataFrame | list,
+               box_dat: pd.DataFrame = None,
+               box_meth: bool | dict = True,
+               bins: int | list = 10):
+    
+    # x dat - dataframe of x data to bin by or list for box_dat
+    # y dat - dataframe of y data to calculate stats on or list for box_dat
+    # box_dat - dataframe of data
+    # box method - how the boxes will be calculated
+    
+    
+    
+    
+    
+    return 0 

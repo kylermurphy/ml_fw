98857160d35aea2d81320a2cb0333a1257c98737
kylermurphy <34041071+kylermurphy@users.noreply.github.com>
2024-09-20 16:16:25 -0400

    testing updates
    New testing and moved old testing

--- ml_fw/resid.py
+++ ml_fw/resid.py
@@ -10,13 +10,18 @@ ivestigate residuals of a ml model
 """
 
 import pandas as pd
+import numpy as np
+from scipy import stats
+
 
 
 def boxplot_vx(x_dat: pd.DataFrame | list, 
                y_dat: pd.DataFrame | list,
                box_dat: pd.DataFrame = None,
                box_meth: bool | dict = True,
-               bins: int | list = 10):
+               bins: int | list = 10,
+               xrange: list[tuple[float, float]] | None = None, 
+               whisker: float = 1.5):
     
     # x dat - dataframe of x data to bin by or list for box_dat
     # y dat - dataframe of y data to calculate stats on or list for box_dat
@@ -24,7 +29,94 @@ def boxplot_vx(x_dat: pd.DataFrame | list,
     # box method - how the boxes will be calculated
     
     
+    # if both f_dat and y_dat are lists then they contain
+    # the column names of the data which we are generating
+    # the box plot vx for
     
+    if isinstance(x_dat, list) \
+        and isinstance(y_dat, list) \
+            and isinstance(box_dat, pd.DataFrame):
+                x_col = x_dat
+                y_col = y_dat
+                
+                x_val = box_dat[x_col]
+                y_val = box_dat[y_col]
+    elif isinstance(x_dat, (pd.DataFrame, pd.Series)) \
+        and isinstance(y_dat, (pd.DataFrame, pd.Series)):
+            # get the column/series name
+            if isinstance(x_dat, pd.DataFrame):
+                x_col = x_dat.columns
+            else:
+                x_col = [x_dat.name] if x_dat.name else ['x_col']
+            # get the columns/series name
+            if isinstance(y_dat, pd.DataFrame):
+                y_col = y_dat.columns
+            else:
+                y_col = y_dat.name if y_dat.name else 'y_col'
+            
+            # get the data
+            x_val = x_dat
+            y_val = y_dat
     
+            
+
+        
+        
+    # now we want to itterate over the x columns
+    # and generate the box plot as binned statistic
     
-    return 0 
+    # define lambdas to calculate the upper and lower
+    # quartiles
+    lq_nan = lambda stat: np.nanpercentile(stat, 25)
+    uq_nan = lambda stat: np.nanpercentile(stat, 75)
+       
+    box_idx = {}
+    
+    for idx in x_col:
+        print(idx)
+        # calculate the statistics as a function of idx
+        try:
+            x = x_val[idx].to_numpy().squeeze()
+        except:
+            x = x_val.to_numpy().squeeze()
+            
+        try:
+            y = y_val[y_col].to_numpy().squeeze()
+        except:
+            x = x_val.to_numpy().squeeze()
+        
+        mean, x_edge, _ = stats.binned_statistic(x, y, bins=bins, 
+                                                 range=xrange, 
+                                                 statistic=np.nanmean)
+        median, _, _ = stats.binned_statistic(x, y,bins=bins, 
+                                              range=xrange, 
+                                              statistic=np.nanmedian)
+        low_q, _, _ = stats.binned_statistic(x, y, bins=bins, 
+                                             range=xrange, statistic=lq_nan)
+        up_q, _, _ = stats.binned_statistic(x, y, bins=bins, 
+                                            range=xrange, statistic=uq_nan)
+        
+        x_cen = (x_edge[0:-1]+[x_edge[1:]])/2.
+        x_cen = x_cen.squeeze()
+        x_wid = x_edge[1]-x_edge[0]
+        # create a list to hold the required
+        # parameters to draw a box and whisker plot
+        box_stats = [ ]
+        for mn, md, lq, uq, in zip(mean, median, low_q, up_q):
+            val = {
+                "mean":  mn,  # not required
+                "med": md,
+                "q1": lq,
+                "q3": uq,
+                "whislo": lq - whisker*(uq-lq),  # required
+                "whishi": uq + whisker*(uq-lq),  # required
+                "fliers": []  # required if showfliers=True
+                }
+            box_stats.append(val)
+        
+        # create a dictionary to store everything needed for plotting
+        # and add it to the return dictionary
+        box_idx[idx] = {'box_stats':box_stats, 'x_edge':x_edge, 
+                        'x_centre':x_cen, 'x_width':x_wid}
+    
+    return box_idx

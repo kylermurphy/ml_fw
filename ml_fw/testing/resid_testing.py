1c366ca10705b286919fae2a19c0b9f85c0135a1
kylermurphy <34041071+kylermurphy@users.noreply.github.com>
2024-09-20 16:17:07 -0400

    Update resid_testing.py
    some plotting

--- ml_fw/testing/resid_testing.py
+++ ml_fw/testing/resid_testing.py
@@ -23,3 +23,16 @@ x_dat = ['AE','SYM_H index']
 y_dat = ['resid']
 
 box_dat = resid.boxplot_vx(dat[x_dat], dat[y_dat])
+
+fig3, ax3 = plt.subplots(2,2, figsize=(8,6))
+
+ae = box_dat['AE']
+
+a = ae['box_stats']
+b = ae['x_edge']
+c = ae['x_centre']
+d = ae['x_width']
+
+
+
+box_stats, x_edge, x_cen, x_wid

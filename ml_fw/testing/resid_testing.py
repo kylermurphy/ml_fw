9cbd6c45665ced227ec90b10eb49428b48efa989
kylermurphy <34041071+kylermurphy@users.noreply.github.com>
2024-10-01 11:55:19 -0400

    Update resid_testing.py
    Some additional testing and simplified plotting.

--- ml_fw/testing/resid_testing.py
+++ ml_fw/testing/resid_testing.py
@@ -16,7 +16,7 @@ from ml_fw import resid as resid
 showmean=True
 whisker=0.0
 
-srange = [-200,10, 4]
+srange = [-200,10]
 sbins = 21
 aerange = [0,2000]
 aebins=20
@@ -32,33 +32,46 @@ y_dat = ['resid']
 box_dat = resid.boxplot_vx(dat[x_dat], dat[y_dat],whisker=whisker, 
                            bins=[aebins,sbins], xrange=[aerange,srange])
 
-fig, ax = plt.subplots(1,1, figsize=(8,6))
+fig, ax = plt.subplots(3,1, figsize=(8,9))
 
 ae_box = box_dat['AE']
 y_val = ae_box['box_stats']
 x_val = ae_box['x_centre']
 x_width = ae_box['x_width']
 
-# use the matplotlib boxplot to plot 
-b1 = ax.bxp(y_val, positions=x_val, widths=x_width, 
+# use the matplotlib boxplot to plot
+
+cc = [1,0,0]
+bx_a = 0.25
+ln_a = 1.0
+ln_w = 2.0
+ 
+b1 = ax[0].bxp(y_val, positions=x_val, widths=x_width, 
                   patch_artist=True, showmeans=showmean, 
-                  shownotches=False, showcaps=False)
+                  shownotches=False, showcaps=False, 
+                  boxprops={'ec':cc+[ln_a], 'fc':cc+[bx_a]},
+                  medianprops={'c':cc, 'lw':ln_w}, 
+                  meanprops={'mec':cc, 'mfc':cc})
 
-ax.set_xticks(x_val,x_val.astype(int).astype(str),rotation=45)
+ax[0].set_xticks(x_val,x_val.astype(int).astype(str),rotation=45)
+ax[0].set_ylabel('Residuals')
+ax[0].set_xlabel('AE')
 
-# make the plot prettier
-color = (1,0,0,0.5)
-for patch, l_md, l_mn in zip(b1['boxes'], b1['medians'], b1['means']):
-    # change patch color
-    patch.set_facecolor([1,0,0,0.25])
-    patch.set_edgecolor([1,0,0,1])
-    #patch.set_alpha(0.25)
-    # change median and mean color
-    l_md.set_color(color)
-    l_md.set_linewidth(2)
-    l_md.set_linestyle('solid')
-    l_mn.set_markerfacecolor(color)
-    l_mn.set_markeredgecolor(color)
-
-
-#box_stats, x_edge, x_cen, x_wid
+# loop through the dictionary to plot all box_plots
+pc = [cc, [0,0,1]]
+for (key, value), ap, bc, in zip(box_dat.items(), ax[1:], pc):
+    print(key)
+    plt_box = box_dat[key]
+    y_val = plt_box['box_stats']
+    x_val = plt_box['x_centre']
+    x_width = plt_box['x_width']
+    
+    b = ap.bxp(y_val, positions=x_val, widths=x_width, 
+                      patch_artist=True, showmeans=showmean, 
+                      shownotches=False, showcaps=False, 
+                      boxprops={'ec':bc+[ln_a], 'fc':bc+[bx_a]},
+                      medianprops={'c':bc, 'lw':ln_w}, 
+                      meanprops={'mec':bc, 'mfc':bc})
+    ap.set_xticks(x_val,x_val.astype(int).astype(str),rotation=45)
+    ap.set_ylabel('Residuals')
+    ap.set_xlabel(key)

485ef41d6b57543fb2d00b703a322aa7b8027d4e
kylermurphy <34041071+kylermurphy@users.noreply.github.com>
2024-10-04 15:50:06 -0400

    Update resid_testing.py
    NBew testing

--- ml_fw/testing_scripts/resid_testing.py
+++ ml_fw/testing_scripts/resid_testing.py
@@ -75,3 +75,27 @@ for (key, value), ap, bc, in zip(box_dat.items(), ax[1:], pc):
     ap.set_xticks(x_val,x_val.astype(int).astype(str),rotation=45)
     ap.set_ylabel('Residuals')
     ap.set_xlabel(key)
+
+    
+box2 = insp.boxplot_vx(['AE'], ['resid'], dat,bins=20, xrange=aerange)  
+
+fig2, ax2 = plt.subplots(1,1, figsize=(8,9))
+
+ae_box = box2['AE']
+y_val = ae_box['box_stats']
+x_val = ae_box['x_centre']
+x_width = ae_box['x_width']
+
+# use the matplotlib boxplot to plot
+
+cc = [1,0,0]
+bx_a = 0.25
+ln_a = 1.0
+ln_w = 2.0
+ 
+b1 = ax2.bxp(y_val, positions=x_val, widths=x_width, 
+                  patch_artist=True, showmeans=showmean, 
+                  shownotches=False, showcaps=False, 
+                  boxprops={'ec':cc+[ln_a], 'fc':cc+[bx_a]},
+                  medianprops={'c':cc, 'lw':ln_w}, 
+                  meanprops={'mec':cc, 'mfc':cc})   

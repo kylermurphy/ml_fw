a3dd638e2d662c1b6881ff5ab30aa901ec4b8cbe
Kyle Murphy <murphy.kyle.r@gmail.com>
2024-10-08 15:18:49 -0400

    Create roll_testing.py
    testing rolling metric

---
+++ ml_fw/testing_scripts/roll_testing.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  8 14:16:09 2024
+
+@author: murph
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from ml_fw import inspect as insp
+import sklearn.metrics as metrics
+
+fn = "C:/Users/murph/OneDrive/Phys4501/data/RF_FI_GEO_OOS_CHAMP.hdf5"
+dat = pd.read_hdf(fn)
+
+y_true = '400kmDensity'
+y_pred = '400kmDensity_pred'
+on = 'DateTime'
+rkwargs = {'window':'90min','center':False}
+met = {'hi':lambda y_true, y_pred: metrics.median_absolute_error(y_true, y_pred),
+       'hizzle':lambda y_true, y_pred: metrics.mean_absolute_error(y_true, y_pred)}
+
+sdate = '2005-08-21'
+edate = '2005-08-30'
+
+gd = (dat['DateTime'] > sdate) & (dat['DateTime'] <= edate)
+
+rdat = dat[gd].copy()
+
+met_df = insp.rolling_met(rdat,y_true=y_true,y_pred=y_pred,on=on,
+                         roll_metric=met, roll_kwargs=rkwargs)
